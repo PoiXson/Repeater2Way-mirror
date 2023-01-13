d664512b45576a8b473c1af1da74e2d2afd5ef8a
lorenzop <lorenzo@poixson.com>
2023-01-13 07:26:35 -0500

    updated to use new xJavaPlugin class

--- src/com/poixson/repeater2way/Repeater2WayPlugin.java
+++ src/com/poixson/repeater2way/Repeater2WayPlugin.java
@@ -3,17 +3,14 @@ package com.poixson.repeater2way;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 
-import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.HandlerList;
-import org.bukkit.plugin.java.JavaPlugin;
 
-import com.poixson.commonmc.pxnCommonPlugin;
-import com.poixson.tools.AppProps;
+import com.poixson.commonmc.tools.plugin.xJavaPlugin;
 
 
-public class Repeater2WayPlugin extends JavaPlugin {
+public class Repeater2WayPlugin extends xJavaPlugin {
 	public static final String LOG_PREFIX  = "[Repeater2Way] ";
 	public static final String CHAT_PREFIX = ChatColor.AQUA + LOG_PREFIX + ChatColor.WHITE;
 	public static final Logger log = Logger.getLogger("Minecraft");
@@ -21,25 +18,20 @@ public class Repeater2WayPlugin extends JavaPlugin {
 	public static final int BSTATS_PLUGIN_ID = 17260;
 
 	protected static final AtomicReference<Repeater2WayPlugin> instance = new AtomicReference<Repeater2WayPlugin>(null);
-	protected static final AtomicReference<Metrics>            metrics  = new AtomicReference<Metrics>(null);
-	protected final AppProps props;
 
 	protected final AtomicReference<RedstoneRepeaterListener> repeaterListener = new AtomicReference<RedstoneRepeaterListener>(null);
 
 
 
 	public Repeater2WayPlugin() {
-		try {
-			this.props = AppProps.LoadFromClassRef(Repeater2WayPlugin.class);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+		super(Repeater2WayPlugin.class);
 	}
 
 
 
 	@Override
 	public void onEnable() {
+		super.onEnable();
 		if (!instance.compareAndSet(null, this))
 			throw new RuntimeException("Plugin instance already enabled?");
 		{
@@ -51,30 +43,11 @@ public class Repeater2WayPlugin extends JavaPlugin {
 				.registerEvents(listener, this);
 			listener.start();
 		}
-		// bStats
-		System.setProperty("bstats.relocatecheck","false");
-		metrics.set(new Metrics(this, BSTATS_PLUGIN_ID));
-		// update checker
-		pxnCommonPlugin.GetPlugin()
-			.getUpdateCheckManager()
-				.addPlugin(this, SPIGOT_PLUGIN_ID, this.getPluginVersion());
 	}
 
 	@Override
 	public void onDisable() {
-		// update checker
-		pxnCommonPlugin.GetPlugin()
-			.getUpdateCheckManager()
-				.removePlugin(SPIGOT_PLUGIN_ID);
-		// stop schedulers
-		try {
-			Bukkit.getScheduler()
-				.cancelTasks(this);
-		} catch (Exception ignore) {}
-		// stop listeners
-		HandlerList.unregisterAll(this);
-		if (!instance.compareAndSet(this, null))
-			throw new RuntimeException("Disable wrong instance of plugin?");
+		super.onDisable();
 		// restore repeaters
 		{
 			final RedstoneRepeaterListener listener = this.repeaterListener.get();
@@ -85,8 +58,17 @@ public class Repeater2WayPlugin extends JavaPlugin {
 
 
 
-	public String getPluginVersion() {
-		return this.props.version;
+	// -------------------------------------------------------------------------------
+
+
+
+	@Override
+	protected int getSpigotPluginID() {
+		return SPIGOT_PLUGIN_ID;
+	}
+	@Override
+	protected int getBStatsID() {
+		return BSTATS_PLUGIN_ID;
 	}
 
 
